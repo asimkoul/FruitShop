c7410a4ffbf7480c62695b5a845e5491c1501191
asimkoul <ahmadasim080gmail.com>
2024-03-07 16:22:28 +0530

    fifthcommit(task)

--- app.js
+++ app.js
@@ -1,26 +1,17 @@
-const mainHeading=document.querySelector("#main-heading")
-mainHeading.style.textAlign="right"
-const basketHeading=document.querySelector("#basket-heading")
-basketHeading.style.color="brown"
+const heading=document.createElement("h3")
+const headingText=document.createTextNode("Buy high quality organic fruits online")
+heading.appendChild(headingText);
+heading.style.fontStyle="italic";
+const div=document.getElementsByTagName("div")
+const firstDiv=div[0]
+firstDiv.appendChild(heading)
+
+const para=document.createElement("p")
+const paraText=document.createTextNode("Total fruits: 4")
+para.appendChild(paraText);
+const secondDiv=div[1]
+secondDiv.appendChild(para)
+
 const fruits=document.querySelector(".fruits")
-fruits.style.backgroungColor="grey"
-
-
-const fruitItems=document.querySelectorAll(".fruit")
-for (let i=0;i<fruitItems.length;i++){
-  if(i%2==0){
-      fruitItems[i].style.backgroundColor="lightgrey"
-  }
-  else{
-    fruitItems[i].style.backgroundColor="brown"
-    fruitItems[i].style.color="white"
-  }
-  fruitItems[i].style.listStyleType="none"
-  fruitItems[i].style.padding="30px"
-    fruitItems[i].style.margin="10px"
-    fruitItems[i].style.borderRadius="5px"
-  
-
-
-  
-}
+secondDiv.insertBefore(para,fruits)
+para.id="fruits-total"
