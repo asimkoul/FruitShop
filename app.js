24fde046dd263d43aecfa5f505df079235b47955
asimkoul <ahmadasim080gmail.com>
2024-03-07 12:06:48 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const header= document.getElementById("header");
+
+header.style.backgroundColor="green"
+header.style.borderBottom="solid orange"
+const mainHeading=document.getElementById("main-heading");
+mainHeading.textContent="Fruit World"
+mainHeading.style.color="orange";
+const basketheading= document.getElementById("basket-heading");
+basketheading.style.color="green"
+const thanks=document.getElementById("thanks")
+thanks.innerHTML="<p>Please visit us again</p>"
