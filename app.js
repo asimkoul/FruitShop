77b4b9a83559f5b4d087d68cd674414c66a784e8
asimkoul <ahmadasim080gmail.com>
2024-03-07 14:18:09 +0530

    fourthcommit(task)

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
-const li=document.getElementsByTagName("li")
-li[4].style.color="blue"
-for(let i=0;i<li.length;i++){
-  li[i].style.fontStyle="italic"
+const mainHeading=document.querySelector("#main-heading")
+mainHeading.style.textAlign="right"
+const basketHeading=document.querySelector("#basket-heading")
+basketHeading.style.color="brown"
+const fruits=document.querySelector(".fruits")
+fruits.style.backgroungColor="grey"
+
+
+const fruitItems=document.querySelectorAll(".fruit")
+for (let i=0;i<fruitItems.length;i++){
+  if(i%2==0){
+      fruitItems[i].style.backgroundColor="lightgrey"
+  }
+  else{
+    fruitItems[i].style.backgroundColor="brown"
+    fruitItems[i].style.color="white"
+  }
+  fruitItems[i].style.listStyleType="none"
+  fruitItems[i].style.padding="30px"
+    fruitItems[i].style.margin="10px"
+    fruitItems[i].style.borderRadius="5px"
+  
+
+
+  
 }
