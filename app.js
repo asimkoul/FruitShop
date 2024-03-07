7dbc61814468bd198688d323fc80742fb9f02258
asimkoul <ahmadasim080gmail.com>
2024-03-07 12:13:02 +0530

    second commit(task)

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
-const header= document.getElementById("header");
-
-header.style.backgroundColor="green"
-header.style.borderBottom="solid orange"
-const mainHeading=document.getElementById("main-heading");
-mainHeading.textContent="Fruit World"
-mainHeading.style.color="orange";
-const basketheading= document.getElementById("basket-heading");
-basketheading.style.color="green"
-const thanks=document.getElementById("thanks")
-thanks.innerHTML="<p>Please visit us again</p>"
+const fruit=document.getElementsByClassName("fruit")
+fruit[2].style.backgroundColor="yellow"
+for (let i=0;i<fruit.length;i++){
+  fruit[i].style.fontWeight="bold"
+}
